365e3a5a61e0f949f3063962be0cb982a63c2ecd
decqq <decbartosz1@gmail.com>
2015-06-16 18:55:38 +0200

    script.js
    Added some logic and functions those will help with handling the game.

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,5 +1,40 @@
 (function () {
+
+	// checks if there is a winner
+	function isWon (map) {
+		var digit = 1
+		for (var i = 0; i < 2; i++, digit = 2) {
+			if((map[0][0] == digit && map[0][1] == digit && map[0][2] == digit)
+			|| (map[1][0] == digit && map[1][1] == digit && map[1][2] == digit)
+			|| (map[2][0] == digit && map[2][1] == digit && map[2][2] == digit)
+
+			|| (map[0][0] == digit && map[1][0] == digit && map[2][0] == digit)
+			|| (map[0][1] == digit && map[1][1] == digit && map[2][1] == digit)
+			|| (map[0][2] == digit && map[1][2] == digit && map[2][2] == digit)
+
+			|| (map[0][0] == digit && map[1][1] == digit && map[2][2] == digit)
+			|| (map[0][2] == digit && map[1][1] == digit && map[2][0] == digit))
+				return digit;
+		}
+
+		return 0;	// no one won so far
+	}
+
+	function isFull (map) {
+		for (var i = 0; i < 3; i++)
+			for (var j = 0; j < 3; j++)
+				if(map[i][j] == 0)
+					return false;
+		return true;
+	}
 	
-	// to be aded
+	// 0 - empty space
+	// 1 - O (computer)
+	// 2 - X (player)
+	var map = [
+		[0, 0, 0],
+		[0, 0, 0],
+		[0, 0, 0]
+	];
 
 })();
